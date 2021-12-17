d7578e59656154b71c10fe23a27a6ecbf74d7057
caalix <mariocalix02@gmail.com>
2021-12-17 14:20:34 -0600

    carro salto

--- src/examen2lab10p2_mariocalix/Salto.java
+++ src/examen2lab10p2_mariocalix/Salto.java
@@ -5,10 +5,38 @@
  */
 package examen2lab10p2_mariocalix;
 
+import java.util.Random;
+
 /**
  *
  * @author mario
  */
 public class Salto extends Carro{
     //cantidad de mnetros que puede saltar aleatorio
+    
+    private int metros;
+    
+    Random random = new Random();
+    int n = random.nextInt(200);
+
+
+    public Salto(int metros, String nombre, int derrape, int velocidad, int ataque, int vida) {
+        super(nombre, derrape, velocidad, ataque, vida);
+        this.metros = metros;
+    }
+
+    public int getMetros() {
+        return metros;
+    }
+
+    public void setMetros(int metros) {
+        this.metros = metros+n;
+    }
+
+
+    
+
+    
+    
+    
 }
